cfe206efef4111b3b8ef7a3c0af34dcda3dc5b7b
Oleksandr M <alextax@ukr.net>
2021-12-26 18:37:46 +0200

    1

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int bonus ;
+        int discount = 10;
+        int purchaseItem = 5;
+        int limit = 3;
+        bonus = purchaseItem > limit ? discount : 0;
+        System.out.println("Your discount is " + bonus + "");
+    }
+}
