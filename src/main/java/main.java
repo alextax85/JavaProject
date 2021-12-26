0a1e7bf9a2961c568054290847954ac3d89dcbae
Oleksandr M <alextax@ukr.net>
2021-12-26 19:36:37 +0200

    1

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,13 @@
 public class main {
     public static void main(String[] args) {
-    int x=0;
-    while (true) {
-        x++;
-        System.out.println(x);
+   int [] array;
+    array = new int[10];
+    for (int i = 0; i < 10; i++ ){
+            array[i] = 100 + i;
+        System.out.println(array[1]);
         }
+
+
             }
 
 }
