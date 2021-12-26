23ccb8b969e6a6a3affd306ef5adba40969227dd
alextax85 <75691088+alextax85@users.noreply.github.com>
2021-12-26 19:38:33 +0300

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
         int bonus ;
-        int discount = 10;
+        int discount = 15;
         int purchaseItem = 5;
         int limit = 3;
         bonus = purchaseItem > limit ? discount : 0;
